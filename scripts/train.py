c0f8d264a56408e23fef9ea718e84ff0e80364e0
Iñaki Salcedo <iniaki.salcedo@gmail.com>
2025-01-23 14:53:34 +1100

    Bug fix

--- scripts/train.py
+++ scripts/train.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
